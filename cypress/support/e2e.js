4fb622aa24d4486ffeb322982ad4c5092a29d25b
Siddharth263 <priyanshulal2213@gmail.com>
2023-02-06 21:44:54 +0530

    'added error message'

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -19,7 +19,7 @@ import './commands';
 Cypress.on('uncaught:exception', (err, runnable) => {
   // we expect a 3rd party library error with message 'list not defined'
   // and don't want to fail the test so we return false
-  if (err.message.includes('list not defined')) {
+  if (err.message.includes('Cannot read properties of undefined (reading \'text\')')) {
     return false
   }
   // we still want to ensure there are no other unexpected
